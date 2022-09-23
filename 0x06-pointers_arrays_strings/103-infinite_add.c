99aafa16e3741b29eaf19570b76b62ffa44a13a7
cybermaxi <maximusagbe@gmail.com>
2022-09-23 17:57:23 +0000

    a function that adds two numbers.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,31 +1,47 @@
-#include<stdio.h>
+#include "main.h"
+
 /**
- * print_number - prints an integer
- * @n:.input integer parameter
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer for result
+ * @size_r: buffer size
  *
- *
+ * Return: address of r or 0
  */
-float addition(float num1, float num2)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-// declare variable
-float sum;
-// calculate sum value
-sum = num1 + num2;
-// return result
-return sum;
-}
+	int i, j, k, l, m, n;
 
-int main()
-{
-// declare variables
-float number1, number2, result;
-
-// take input
-printf("Enter two number: ");
-scanf("%f %f",&number1, &number2);
-// find addition of two numbers
-result = addition(number1, number2);
-// display result
-printf("%.2f + %.2f = %.2f\n",number1, number2, result);
-return 0;
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
+		return (0);
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	{
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
+		{
+			break;
+		}
+		m = n / 10;
+		r[k] = n % 10 + '0';
+	}
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+		return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
+	}
+	return (r);
 }
