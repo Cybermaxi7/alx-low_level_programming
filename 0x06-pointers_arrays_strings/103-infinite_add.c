8c9a86ea21ad5d3ef41bfa300823a9bd33905e5a
cybermaxi <maximusagbe@gmail.com>
2022-09-23 17:55:50 +0000

    a function that adds two numbers.

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/**
+ * print_number - prints an integer
+ * @n:.input integer parameter
+ *
+ *
+ */
+float addition(float num1, float num2)
+{
+// declare variable
+float sum;
+// calculate sum value
+sum = num1 + num2;
+// return result
+return sum;
+}
+
+int main()
+{
+// declare variables
+float number1, number2, result;
+
+// take input
+printf("Enter two number: ");
+scanf("%f %f",&number1, &number2);
+// find addition of two numbers
+result = addition(number1, number2);
+// display result
+printf("%.2f + %.2f = %.2f\n",number1, number2, result);
+return 0;
+}
